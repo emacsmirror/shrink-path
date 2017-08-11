3b2b72a5467d364bc0c6d3eb4c6314a53ef321e7
Benjamin Andresen <bandresen@gmail.com>
2017-08-11 09:18:50 +0200

    Add: tests: introduce with-playground & when guard load-file
    with-playground: limitation is that it works fine with a nesting depth of 1 but
    buttercup's describe macros reset the HOME env

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,12 +1,22 @@
 ;;; test-helper.el --- description -*- lexical-binding: t; -*-
 
-(defvar shrink-path-test/test-path
-  (directory-file-name (file-name-directory load-file-name))
-  "Path to tests directory.")
+;; only eval non-interactively
+(when load-file-name
+  (defvar shrink-path-test/test-path
+    (directory-file-name (file-name-directory load-file-name))
+    "Path to tests directory.")
 
-(defvar shrink-path-test/root-path
-  (directory-file-name (file-name-directory shrink-path-test/test-path))
-"Path to root directory.")
+  (defvar shrink-path-test/root-path
+    (directory-file-name (file-name-directory shrink-path-test/test-path))
+    "Path to root directory.")
+
+  (defvar shrink-path-test/playground-path
+    (expand-file-name "playground" shrink-path-test/test-path)
+    "Path to playground directory")
+
+  (defvar shrink-path-el (expand-file-name "shrink-path.el" shrink-path-test/root-path))
+
+  (load shrink-path-el 'noerror 'nomessage))
 
 (defmacro with-home (home &rest body)
   "Within HOME environment evaluate BODY."
@@ -19,6 +29,14 @@
            ,@body)
        (setenv "HOME" before))))
 
-(defvar shrink-path-el (expand-file-name "shrink-path.el" shrink-path-test/root-path))
-
-(load shrink-path-el 'noerror 'nomessage)
+(defmacro with-playground (&rest body)
+  "Sets up empty playground directory and yields BODY in this environment.
+PLAYGROUND-PATH is available in BODY."
+  (declare (indent 0))
+  `(unwind-protect
+       (progn
+         (let ((playground-path shrink-path-test/playground-path))
+           (make-directory shrink-path-test/playground-path)
+           (with-home shrink-path-test/playground-path
+             ,@body)))
+     (delete-directory shrink-path-test/playground-path 'recursive)))
