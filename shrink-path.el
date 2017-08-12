532702e2705527147140536b2800915d83e77ee4
Benjamin Andresen <bandresen@gmail.com>
2017-08-12 14:05:17 +0200

    Document: $project: add Commentary

--- shrink-path.el
+++ shrink-path.el
@@ -21,12 +21,20 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; along with GNU Emacs; see the file LICENSE.  If not, write to the
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
-;;; No commentary
+
+;; Provides functions that offer fish shell[1] path truncation.
+;; Directory /usr/share/emacs/site-lisp => /u/s/e/site-lisp
+;;
+;; Also includes utility functions that make integration in eshell or the
+;; modeline easier.
+;;
+;; [1] https://fishshell.com/
+
 
 ;;; Code:
 (require 'dash)
